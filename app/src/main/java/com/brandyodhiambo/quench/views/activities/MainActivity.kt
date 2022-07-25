14697ee07f9fe814ae19688f9f7120b78940b5fd
brandy-kay <brandyariana99@gmail.com>
2022-07-25 15:00:27 +0300

    added splash screen

--- app/src/main/java/com/brandyodhiambo/quench/views/activities/MainActivity.kt
+++ app/src/main/java/com/brandyodhiambo/quench/views/activities/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.brandyodhiambo.quench.views.screens.splash.NavGraphs
 import com.brandyodhiambo.quench.views.ui.theme.QuenchTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    QuenchTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/brandyodhiambo/quench/views/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/brandyodhiambo/quench/views/screens/splash/SplashScreen.kt
@@ -1,2 +1,29 @@
 package com.brandyodhiambo.quench.views.screens.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.brandyodhiambo.quench.R
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination(start = true)
+@Composable
+fun SplashScreen() {
+    Column(
+        Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_blue_mineral_water),
+            contentDescription = null
+        )
+    }
+}
