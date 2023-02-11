7e609d01c5a9ee98cc29821e35dbe47f87020ebf
brandy-kay <brandyariana99@gmail.com>
2023-02-11 14:18:49 +0300

    completed migration to dsl

---
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -0,0 +1,7 @@
+object AndroidConfig{
+    const val applicationId = "com.brandyodhiambo.quench"
+    const val minSdk = 21
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
