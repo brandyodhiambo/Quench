7fa1aeb360a22fa0e4e3c8c9c3abff40b3e94b99
brandy-kay <brandyariana99@gmail.com>
2023-02-18 10:44:54 +0300

    divided dependencies

--- feature/statistics/build.gradle.kts
+++ feature/statistics/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp") version ("1.7.10-1.0.6")
+}
+
+apply {
+    from("$rootDir/shared_dependencies.gradle")
 }
 
 android {
     namespace = "com.brandyodhiambo.statistics"
     compileSdk = 33
+
 
     defaultConfig {
         minSdk = 24
@@ -34,11 +43,26 @@ android {
 }
 
 dependencies {
+    // RamCosta Navigation
+    implementation("io.github.raamcosta.compose-destinations:core:1.5.20-beta")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.5.15-beta")
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Navigation animation
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.24.7-alpha")
+}
+
+kotlin {
+    sourceSets {
+        debug {
+            kotlin.srcDir("build/generated/ksp/debug/kotlin")
+        }
+        release {
+            kotlin.srcDir("build/generated/ksp/release/kotlin")
+        }
+    }
+}
+
+ksp {
+    arg("compose-destinations.mode", "destinations")
+    arg("compose-destinations.moduleName", "statistics")
 }
