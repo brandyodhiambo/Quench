20714995955b0e0444fc5b36e9f372efcba783ed
brandy-kay <brandyariana99@gmail.com>
2023-08-01 12:17:47 +0300

    working on statistics screen

--- feature/statistics/src/main/java/com/brandyodhiambo/statistics/presentation/StatisticsScreen.kt
+++ feature/statistics/src/main/java/com/brandyodhiambo/statistics/presentation/StatisticsScreen.kt
@@ -3,17 +3,22 @@ package com.brandyodhiambo.statistics.presentation
 import android.annotation.SuppressLint
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,8 +30,10 @@ import androidx.compose.ui.unit.sp
 import com.brandyodhiambo.common.R
 import com.brandyodhiambo.designsystem.theme.blackColor
 import com.brandyodhiambo.designsystem.theme.primaryColor
-import com.brandyodhiambo.quench.views.composables.barChartDataMonths
-import com.brandyodhiambo.quench.views.composables.verticalAxisValues
+import com.brandyodhiambo.statistics.presentation.component.barChartDataMonth
+import com.brandyodhiambo.statistics.presentation.component.barChartDataWeek
+import com.brandyodhiambo.statistics.presentation.component.barChartDataYear
+import com.brandyodhiambo.statistics.presentation.component.verticalAxisValues
 import com.mahmoud.composecharts.barchart.BarChart
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -35,15 +42,19 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 @Destination
 @Composable
 fun StatisticsScreen(
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
 ) {
+    val setGraphDaily = remember { mutableStateOf(true) }
+    val setGraphMonthly = remember { mutableStateOf(false) }
+    val setGraphYearly = remember { mutableStateOf(false) }
+
     Scaffold(
-        backgroundColor = primaryColor
+        backgroundColor = primaryColor,
     ) { paddingValues ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(paddingValues)
+                .padding(paddingValues),
         ) {
             LazyColumn {
                 item {
@@ -51,13 +62,127 @@ fun StatisticsScreen(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(top = 16.dp, start = 16.dp, end = 16.dp),
-                        elevation = 4.dp
+                        elevation = 4.dp,
                     ) {
-                        BarChart(
-                            modifier = Modifier.fillMaxSize().padding(16.dp),
-                            barChartData = barChartDataMonths,
-                            verticalAxisValues = verticalAxisValues
-                        )
+                        Column(
+                            modifier = Modifier.padding(8.dp),
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 8.dp, start = 8.dp, end = 8.dp),
+                                horizontalArrangement = Arrangement.SpaceEvenly,
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .height(30.dp)
+                                        .width(70.dp)
+                                        .background(
+                                            color = if (setGraphDaily.value) {
+                                                primaryColor
+                                            } else {
+                                                primaryColor.copy(
+                                                    alpha = 0.2f,
+                                                )
+                                            },
+                                            shape = RoundedCornerShape(8.dp),
+                                        ).clickable {
+                                            setGraphDaily.value = true
+                                            setGraphMonthly.value = false
+                                            setGraphYearly.value = false
+                                        },
+                                    contentAlignment = Alignment.Center,
+                                ) {
+                                    Text(
+                                        text = "Daily",
+                                        style = MaterialTheme.typography.h6,
+                                        fontWeight = FontWeight.Normal,
+                                        color = blackColor,
+                                    )
+                                }
+                                Box(
+                                    modifier = Modifier
+                                        .height(30.dp)
+                                        .width(70.dp)
+                                        .background(
+                                            color = if (setGraphMonthly.value) {
+                                                primaryColor
+                                            } else {
+                                                primaryColor.copy(
+                                                    alpha = 0.2f,
+                                                )
+                                            },
+                                            shape = RoundedCornerShape(8.dp),
+                                        ).clickable {
+                                            setGraphDaily.value = false
+                                            setGraphMonthly.value = true
+                                            setGraphYearly.value = false
+                                        },
+                                    contentAlignment = Alignment.Center,
+                                ) {
+                                    Text(
+                                        text = "Monthly",
+                                        style = MaterialTheme.typography.h6,
+                                        fontWeight = FontWeight.Normal,
+                                        color = blackColor,
+                                    )
+                                }
+                                Box(
+                                    modifier = Modifier
+                                        .height(30.dp)
+                                        .width(70.dp)
+                                        .background(
+                                            color = if (setGraphYearly.value) {
+                                                primaryColor
+                                            } else {
+                                                primaryColor.copy(
+                                                    alpha = 0.2f,
+                                                )
+                                            },
+                                            shape = RoundedCornerShape(8.dp),
+                                        ).clickable {
+                                            setGraphDaily.value = false
+                                            setGraphMonthly.value = false
+                                            setGraphYearly.value = true
+                                        },
+                                    contentAlignment = Alignment.Center,
+                                ) {
+                                    Text(
+                                        text = "Yearly",
+                                        style = MaterialTheme.typography.h6,
+                                        fontWeight = FontWeight.Normal,
+                                        color = blackColor,
+                                    )
+                                }
+                            }
+                            if (setGraphDaily.value) {
+                                BarChart(
+                                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                                    barChartData = barChartDataWeek,
+                                    verticalAxisValues = verticalAxisValues,
+                                    isShowHorizontalLines = true,
+                                    isShowVerticalAxis = true,
+                                )
+                            }
+                            if (setGraphMonthly.value) {
+                                BarChart(
+                                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                                    barChartData = barChartDataMonth,
+                                    verticalAxisValues = verticalAxisValues,
+                                    isShowHorizontalLines = true,
+                                    isShowVerticalAxis = true,
+                                )
+                            }
+                            if (setGraphYearly.value) {
+                                BarChart(
+                                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                                    barChartData = barChartDataYear,
+                                    verticalAxisValues = verticalAxisValues,
+                                    isShowHorizontalLines = true,
+                                    isShowVerticalAxis = true,
+                                )
+                            }
+                        }
                     }
                 }
                 item {
@@ -78,7 +203,7 @@ val weekAchiement = listOf(
     Weeks(isAcheived = false, "Wed"),
     Weeks(isAcheived = true, "Thu"),
     Weeks(isAcheived = false, "Fri"),
-    Weeks(isAcheived = false, "Sat")
+    Weeks(isAcheived = false, "Sat"),
 )
 
 @Composable
@@ -88,20 +213,20 @@ fun Last7DayGoals() {
             .height(135.dp)
             .padding(top = 8.dp, start = 16.dp, end = 16.dp)
             .fillMaxWidth(),
-        elevation = 4.dp
+        elevation = 4.dp,
     ) {
         Column(
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
         ) {
             Text(
                 text = "Last 7 Days Goals Achieve",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.SemiBold,
-                color = primaryColor
+                color = primaryColor,
             )
             LazyRow(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly
+                horizontalArrangement = Arrangement.SpaceEvenly,
             ) {
                 items(weekAchiement) { weeks ->
                     WeeksAcheive(weeks = weeks)
@@ -113,10 +238,10 @@ fun Last7DayGoals() {
 
 @Composable
 fun WeeksAcheive(
-    weeks: Weeks
+    weeks: Weeks,
 ) {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         if (weeks.isAcheived == true) {
             GoldCup()
@@ -135,7 +260,7 @@ fun GoldCup() {
         modifier = Modifier
             .size(48.dp)
             .padding(4.dp),
-        elevation = 4.dp
+        elevation = 4.dp,
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_cup),
@@ -143,7 +268,7 @@ fun GoldCup() {
             modifier = Modifier
                 .size(48.dp)
                 .padding(8.dp),
-            contentDescription = null
+            contentDescription = null,
         )
     }
 }
@@ -156,7 +281,7 @@ fun BlackCup() {
         modifier = Modifier
             .size(48.dp)
             .padding(4.dp),
-        elevation = 4.dp
+        elevation = 4.dp,
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_black_cup),
@@ -164,7 +289,7 @@ fun BlackCup() {
             modifier = Modifier
                 .size(48.dp)
                 .padding(8.dp),
-            contentDescription = null
+            contentDescription = null,
         )
     }
 }
@@ -175,150 +300,150 @@ fun DrinkWaterReport() {
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 8.dp, start = 16.dp, end = 16.dp),
-        elevation = 4.dp
+        elevation = 4.dp,
     ) {
         Column(
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
         ) {
             Text(
                 text = "Drink Water Report",
                 fontSize = 16.sp,
                 fontWeight = FontWeight.SemiBold,
-                color = primaryColor
+                color = primaryColor,
             )
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Icon(
                         imageVector = Icons.Default.DateRange,
                         tint = primaryColor,
-                        contentDescription = null
+                        contentDescription = null,
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = "Week Avarage",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.W400,
-                        color = blackColor
+                        color = blackColor,
                     )
                 }
                 Text(
                     text = "1850ml/day",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.W400,
-                    color = primaryColor
+                    color = primaryColor,
                 )
             }
             Divider(
                 modifier = Modifier.height(1.dp).padding(start = 8.dp, end = 8.dp),
                 color = Color.Gray,
-                thickness = 1.dp
+                thickness = 1.dp,
             )
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Icon(
                         imageVector = Icons.Default.DateRange,
                         tint = primaryColor,
-                        contentDescription = null
+                        contentDescription = null,
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = "Monthly Avarage",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.W400,
-                        color = blackColor
+                        color = blackColor,
                     )
                 }
                 Text(
                     text = "1450ml/day",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.W400,
-                    color = primaryColor
+                    color = primaryColor,
                 )
             }
             Divider(
                 modifier = Modifier.height(1.dp).padding(start = 8.dp, end = 8.dp),
                 color = Color.Gray,
-                thickness = 1.dp
+                thickness = 1.dp,
             )
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Icon(
                         imageVector = Icons.Default.Star,
                         tint = primaryColor,
-                        contentDescription = null
+                        contentDescription = null,
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = "Avarage Completion",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.W400,
-                        color = blackColor
+                        color = blackColor,
                     )
                 }
                 Text(
                     text = "55%",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.W400,
-                    color = primaryColor
+                    color = primaryColor,
                 )
             }
             Divider(
                 modifier = Modifier.height(1.dp).padding(start = 8.dp, end = 8.dp),
                 color = Color.Gray,
-                thickness = 1.dp
+                thickness = 1.dp,
             )
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                horizontalArrangement = Arrangement.SpaceBetween,
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     Icon(
                         imageVector = Icons.Default.AccountCircle,
                         tint = primaryColor,
-                        contentDescription = null
+                        contentDescription = null,
                     )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = "Drink Frequency",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.W400,
-                        color = blackColor
+                        color = blackColor,
                     )
                 }
                 Text(
                     text = "5 Times/day",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.W400,
-                    color = primaryColor
+                    color = primaryColor,
                 )
             }
         }
@@ -327,5 +452,5 @@ fun DrinkWaterReport() {
 
 data class Weeks(
     val isAcheived: Boolean,
-    val day: String
+    val day: String,
 )
--- feature/statistics/src/main/java/com/brandyodhiambo/statistics/presentation/component/BarChart.kt
+++ feature/statistics/src/main/java/com/brandyodhiambo/statistics/presentation/component/BarChart.kt
@@ -1,30 +1,39 @@
-package com.brandyodhiambo.quench.views.composables
+package com.brandyodhiambo.statistics.presentation.component
 
 import com.brandyodhiambo.designsystem.theme.primaryColor
 import com.mahmoud.composecharts.barchart.BarChartEntity
 
 
-val barChartDataYear = listOf(
-    BarChartEntity(30f, primaryColor, "2018"),
-    BarChartEntity(20f, primaryColor, "2019"),
-    BarChartEntity(100f, primaryColor, "2020"),
-    BarChartEntity(70f, primaryColor, "2021"),
-    BarChartEntity(10f, primaryColor, "2022")
+val barChartDataMonth = listOf(
+    BarChartEntity(30f, primaryColor, "Week 1"),
+    BarChartEntity(20f, primaryColor, "Week 2"),
+    BarChartEntity(100f, primaryColor, "Week 3"),
+    BarChartEntity(70f, primaryColor, "Week 4"),
 )
 
-val barChartDataMonths = listOf(
-    BarChartEntity(30f, primaryColor, "J"),
-    BarChartEntity(50f, primaryColor, "F"),
-    BarChartEntity(20f, primaryColor, "M"),
-    BarChartEntity(80f, primaryColor, "A"),
-    BarChartEntity(20f, primaryColor, "M"),
-    BarChartEntity(90f, primaryColor, "J"),
-    BarChartEntity(05f, primaryColor, "J"),
-    BarChartEntity(100f, primaryColor, "A"),
-    BarChartEntity(40f, primaryColor, "S"),
-    BarChartEntity(70f, primaryColor, "O"),
-    BarChartEntity(70f, primaryColor, "N"),
-    BarChartEntity(70f, primaryColor, "D"),
+val barChartDataWeek = listOf(
+    BarChartEntity(30f, primaryColor, "Sun"),
+    BarChartEntity(20f, primaryColor, "Mon"),
+    BarChartEntity(100f, primaryColor, "Tus"),
+    BarChartEntity(70f, primaryColor, "Wed"),
+    BarChartEntity(90f, primaryColor, "Thirs"),
+    BarChartEntity(60f, primaryColor, "Fri"),
+    BarChartEntity(80f, primaryColor, "Sat"),
+)
+
+val barChartDataYear = listOf(
+    BarChartEntity(30f, primaryColor, "Jan"),
+    BarChartEntity(50f, primaryColor, "Feb"),
+    BarChartEntity(20f, primaryColor, "Mar"),
+    BarChartEntity(80f, primaryColor, "Apr"),
+    BarChartEntity(20f, primaryColor, "May"),
+    BarChartEntity(90f, primaryColor, "Jun"),
+    BarChartEntity(05f, primaryColor, "Jul"),
+    BarChartEntity(100f, primaryColor, "Aug"),
+    BarChartEntity(40f, primaryColor, "Sep"),
+    BarChartEntity(70f, primaryColor, "Oct"),
+    BarChartEntity(70f, primaryColor, "Nov"),
+    BarChartEntity(70f, primaryColor, "Dec"),
 )
 
 val verticalAxisValues =
