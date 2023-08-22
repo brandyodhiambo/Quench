5f28c39e578e57ff55e48bb95f355895b59fb02f
brandy-kay <brandyariana99@gmail.com>
2023-08-22 17:26:57 +0300

    works on settings screen

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     // RamCosta Navigation
     implementation("io.github.raamcosta.compose-destinations:core:1.5.20-beta")
+    implementation(project(mapOf("path" to ":feature:home")))
     ksp("io.github.raamcosta.compose-destinations:ksp:1.5.15-beta")
 
     // Navigation animation
